e0ef7f18b4ed3e0083031016b5c1c7b074f5fc0a
mariano26 <alanmariano26@terra.com.br>
2024-06-27 23:16:59 -0300

    desafio dio felipe

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Teste")
+let nome = "Fawkes"
+let expAtual = 10001
+let nivel
+
+if (expAtual <= 1000) {
+    nivel = "Ferro"
+} else if (expAtual <= 2000) {
+    nivel = "Bronze"
+} else if (expAtual <= 5000) {
+    nivel = "Prata"
+} else if (expAtual <= 7000) {
+    nivel = "Ouro"
+} else if (expAtual <= 8000) {
+    nivel = "Platina"
+} else if (expAtual <= 9000) {
+    nivel = "Ascendente"
+} else if (expAtual <= 10000) {
+    nivel = "Imortal"
+} else {
+    nivel = "Radiante"
+}
+
+
+
+console.log("O Herói de nome " + [nome] + " está no nível de " + [nivel])
